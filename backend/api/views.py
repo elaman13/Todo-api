d489b8d9ba14a441ec2dc9e8d1ee21f3b1bed399
Elyas <elaman99620@gmail.com>
2025-10-24 12:18:45 +0300

    Add JWT Auth

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import generics
+from . import serializers
+from django.contrib.auth.models import User
+from rest_framework.permissions import AllowAny, IsAuthenticated
 
 # Create your views here.
+class SignUpView(generics.CreateAPIView):
+    """
+    User registration view.
+    """
+    queryset = User.objects.all()
+    serializer_class = serializers.SignUpSerializer
+    permission_classes = [AllowAny]
